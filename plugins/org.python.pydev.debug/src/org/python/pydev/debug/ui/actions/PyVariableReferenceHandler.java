1a2e118379ecc3511bc4577bef1ba464be49511c
Hussain Bohra <hussain.bohra@tavant.com>
2011-11-28 16:51:13 +0530

    Removing test code written for converting PyVariable obj to PyValriableCollection

--- plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/actions/PyVariableReferenceHandler.java
+++ plugins/org.python.pydev.debug/src/org/python/pydev/debug/ui/actions/PyVariableReferenceHandler.java
@@ -1,20 +1,12 @@
 package org.python.pydev.debug.ui.actions;
 
 import java.util.Iterator;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.debug.internal.ui.DebugUIPlugin;
-import org.eclipse.debug.internal.ui.views.variables.VariablesView;
-import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.python.pydev.debug.model.PyVariable;
 import org.python.pydev.debug.model.PyVariableCollection;
@@ -38,18 +30,7 @@ public class PyVariableReferenceHandler extends AbstractHandler {
 					((PyVariableCollection) element).initializeReferrers();
 				} else if (element instanceof PyVariable) {
 					// TODO: Identify how to change the tree element from PyVariable to PyVariableCollection
-					final PyVariable variable = (PyVariable) element;
-					Display display = Display.getDefault();
-
-					display.syncExec(new Runnable() {
-						public void run() {
-							List elementList = ((TreeSelection) selection).toList();
-							elementList.remove(variable);
-						}
-					});
-					IWorkbenchPage page = DebugUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
-					IViewPart part = page.findView(IDebugUIConstants.ID_VARIABLE_VIEW);
-					((VariablesView) part).getViewer();
+					PyVariable variable = (PyVariable) element;
 				}
 			}
 		}
